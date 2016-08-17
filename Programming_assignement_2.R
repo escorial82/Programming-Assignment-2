d434fc1d8d5cf157b3c3b41b7bf0552cd16719c9
escorial82 <escorial82@gmail.com>
2016-08-17 23:04:10 +0200

    Add files via upload

---
+++ Programming_assignement_2.R
@@ -0,0 +1,24 @@
+makeCacheMatrix <- function(x = matrix()){
+    im <- NULL
+    set <- function(y) {
+        x <<- y
+        inv <<- NULL
+    }
+    get <- function() x
+    setinverse <- function(solve) im <<- solve
+    getinverse <- function() I'm
+    list(set = set, get=get,setinverse = setinverse,getinverse = get inverse
+}
+
+cacheSolve <- function(x, ...){
+    im <- x$getinverse
+    if(!is.null(im)) {
+        message("getting cached data")
+        return(I'm
+    }
+    sourcematrix <- x$get
+    inv <- solve(sourcematrix)
+    x$setinverse(im)
+    im
+    sourcematrix %*% im    ##This will show the product of the two matrices, i.e. equivalent to 1
+}
